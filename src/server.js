7f21f952dea7062bd78d87a41e1d68ac15dd5dbc
hugolopess1 <lopess1230torres@gmail.com>
2026-05-07 20:25:57 -0300

    cleanup server

--- src/server.js
+++ src/server.js
@@ -1,41 +1,20 @@
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
 
 app.get('/', (req, res) => {
   res.json({ status: 'online' });
 });
 
-app.get('/', (req, res) => {
-  res.json({ status: 'online' });
-});
-
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    status: 'online'
-  });
-});
-
-app.get('/stations', (req, res) => {
+app.get('/stations', async (req, res) => {
   res.json([
     {
       id: 1,
-      name: 'Posto Shell Centro',
-      brand: 'Shell',
-      gasolinePrice: 5.89,
-      ethanolPrice: 4.29,
-      dieselPrice: 5.99,
-      trustScore: 87,
-      avgRating: 4.7,
-      reviewCount: 128
+      name: 'Posto Teste'
     }
   ]);
 });
+
+
 
 const PORT = process.env.PORT || 8080;
 
