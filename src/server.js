77ee2e67a22cd5bddbdf868588bbd6459e199d49
hugolopess1 <lopess1230torres@gmail.com>
2026-05-07 21:15:45 -0300

    corrige frontend

--- src/server.js
+++ src/server.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const app = express();
 
